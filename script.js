d59d1e2d2a404423fd5c656588bcec9311c76ca6
Maxwell Blevins <57766761+maxwellblevins@users.noreply.github.com>
2019-12-17 12:31:45 -0500

    Starting on quiz

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+var question = "What is Charles Babbage's most notable creation?"
+var answers = ['First automatic digital computer', 'The original keyboard', 'First search engine', 'The phone'];
+var answerButtons = document.getElementsByClassName("quizButton");
+for(var spot=0; spot<answers.length; spot++)
+{
+    answeButtons[spot].innerHTML = answers[spot];
+}
