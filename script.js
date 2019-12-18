6112fc57a4850250875b21cfaaaa66ce99f09447
Maxwell Blevins <57766761+maxwellblevins@users.noreply.github.com>
2019-12-18 12:31:57 -0500

    Done

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 var question = "What is Charles Babbage's most notable creation?"
-var answers = ['First automatic digital computer', 'The original keyboard', 'First search engine', 'The phone'];
+var answers = ['First automatic digital computer', 
+                'The original keyboard', 
+                'First search engine', 
+                'The phone'];
 var answerButtons = document.getElementsByClassName("quizButton");
-for(var spot=0; spot<answers.length; spot++)
+var correct = 0;  
+
+document.getElementById("question").innerHTML = question;
+
+for(var spot=0; spot<answers.length; spot++) 
 {
-    answeButtons[spot].innerHTML = answers[spot];
+    answerButtons[spot].innerHTML = answers[spot];
+
+
+    answerButtons[spot].addEventListener('click', function() {
+    if(this.innerHTML == answers[correct]) { 
+        this.setAttribute('style','background-color:rgb(76,175,80);'); 
+    } else { 
+        this.setAttribute('style','backgrund-color:rgb(210,50,35);');
+    }
+    });
 }
+
